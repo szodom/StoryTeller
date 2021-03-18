96aeed6a32a65e6b0c1c8b524417fa9c6cd16833
s545o8 <s545o8@inf.elte.hu>
2021-03-18 18:29:01 +0100

    Create mig file for Book, User_WrittenBook and User_OwnedBook

---
+++ database/migrations/2021_03_18_171527_create_user_ownedbook.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserOwnedbook extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_ownedbook', function (Blueprint $table) {
+            $table->bigInteger('userID');
+            $table->bigInteger('bookID');
+            $table->integer('actualPage');
+
+            //Connection to user table
+            $table->foreign('userID')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            //Connection to book table
+            $table->foreign('bookID')
+                ->references('id')->on('book')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_ownedbook');
+    }
+}
